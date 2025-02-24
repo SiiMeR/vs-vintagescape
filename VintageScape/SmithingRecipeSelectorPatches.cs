dcc34e26cc344cd4eae1c1760dd17b2da25b0f2b
Siim Raudsepp <siimraudsepp@msn.com>
2025-02-24 22:10:46 +0200

    PoC: Cannot smith anything if your metalworking level < 5

---
+++ VintageScape/SmithingRecipeSelectorPatches.cs
@@ -0,0 +1,31 @@
+﻿using XLib.XLeveling;
+
+namespace VintageScape;
+
+public class SmithingRecipeSelectorPatches
+{
+    public static bool OnSlotClickPatch(int num)
+    {
+        var xskills = XSkills.XSkills.Instance.XLeveling;
+        if (xskills == null)
+        {
+            return true;
+        }
+
+        var player = xskills.Api.World.AllPlayers[0];
+        
+        var skillid = xskills.GetSkill("metalworking").Id;
+        var playerSkill = xskills.IXLevelingAPI?.GetPlayerSkillSet(player).PlayerSkills[skillid];
+        if (playerSkill == null)
+        {
+            return true;
+        }
+
+        if (playerSkill.Level < 5)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
--- VintageScape/VintageScapeModSystem.cs
+++ VintageScape/VintageScapeModSystem.cs
@@ -1,6 +1,5 @@
-﻿using Vintagestory.API.Client;
-using Vintagestory.API.Server;
-using Vintagestory.API.Config;
+﻿using HarmonyLib;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.GameContent;
 
@@ -8,20 +7,15 @@ namespace VintageScape;
 
 public class VintageScapeModSystem : ModSystem
 {
-    // Called on server and client
-    // Useful for registering block/entity classes on both sides
-    public override void Start(ICoreAPI api)
-    {
-        api.Logger.Notification("Hello from template mod: " + api.Side);
-    }
-
-    public override void StartServerSide(ICoreServerAPI api)
-    {
-        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("vintagescape:hello"));
-    }
 
     public override void StartClientSide(ICoreClientAPI api)
     {
-        api.Logger.Notification("Hello from template mod client side: " + Lang.Get("vintagescape:hello"));
+        var harmony = new Harmony(Mod.Info.ModID);
+
+        var original =
+            AccessTools.Method(typeof(GuiDialogBlockEntityRecipeSelector), "OnSlotClick");
+        var patch = AccessTools.Method(typeof(SmithingRecipeSelectorPatches), nameof(SmithingRecipeSelectorPatches.OnSlotClickPatch));
+
+        harmony.Patch(original, prefix: new HarmonyMethod(patch));
     }
 }
